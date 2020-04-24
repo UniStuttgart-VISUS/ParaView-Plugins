556a02935f9795d71bb40f9647997c05150fb4ae
Alexander Straub <alexander.straub@visus.uni-stuttgart.de>
2020-04-24 14:47:27 +0200

    Additional output useful for filtering

--- plugins/topology/modules/attachment_separation_lines/attachment_separation_lines.cxx
+++ plugins/topology/modules/attachment_separation_lines/attachment_separation_lines.cxx
@@ -79,7 +79,27 @@ int attachment_separation_lines::RequestData(vtkInformation*, vtkInformationVect
     auto values = vtkSmartPointer<vtkDoubleArray>::New();
     values->SetNumberOfComponents(1);
     values->SetNumberOfTuples(velocities->GetNumberOfTuples());
-    values->SetName("Value");
+    values->SetName("Values");
+
+    auto attachment_values = vtkSmartPointer<vtkDoubleArray>::New();
+    attachment_values->SetNumberOfComponents(1);
+    attachment_values->SetNumberOfTuples(velocities->GetNumberOfTuples());
+    attachment_values->SetName("Attachment values");
+
+    auto separation_values = vtkSmartPointer<vtkDoubleArray>::New();
+    separation_values->SetNumberOfComponents(1);
+    separation_values->SetNumberOfTuples(velocities->GetNumberOfTuples());
+    separation_values->SetName("Separation values");
+
+    auto attachment_eigenvectors = vtkSmartPointer<vtkDoubleArray>::New();
+    attachment_eigenvectors->SetNumberOfComponents(3);
+    attachment_eigenvectors->SetNumberOfTuples(velocities->GetNumberOfTuples());
+    attachment_eigenvectors->SetName("Attachment eigenvectors");
+
+    auto separation_eigenvectors = vtkSmartPointer<vtkDoubleArray>::New();
+    separation_eigenvectors->SetNumberOfComponents(3);
+    separation_eigenvectors->SetNumberOfTuples(velocities->GetNumberOfTuples());
+    separation_eigenvectors->SetName("Separation eigenvectors");
 
     Eigen::Vector3d normal, velocity;
     Eigen::Matrix3d Jacobian;
@@ -124,7 +144,7 @@ int attachment_separation_lines::RequestData(vtkInformation*, vtkInformationVect
         Eigen::Matrix2d projected_Jacobian;
         projected_Jacobian << w1.dot(basis_1), w2.dot(basis_1), w1.dot(basis_2), w2.dot(basis_2);
 
-        // Compute value
+        // Compute value from acceleration
         const auto Ju = projected_Jacobian * projected_velocity;
 
         Eigen::Matrix2d uJu;
@@ -133,6 +153,59 @@ int attachment_separation_lines::RequestData(vtkInformation*, vtkInformationVect
             projected_velocity[1], Ju[1];
 
         values->SetValue(i, uJu.determinant());
+
+        // Compute values from Eigen decomposition, separately for attachment and separation
+        const Eigen::EigenSolver<Eigen::Matrix2d> eigen(projected_Jacobian, true);
+        const auto eigenvalues = eigen.eigenvalues();
+        const auto eigenvectors = eigen.eigenvectors();
+
+        if (eigenvalues.imag().isZero() && eigenvectors.imag().isZero())
+        {
+            Eigen::Vector2d larger, smaller;
+
+            if (eigenvalues[0].real() > eigenvalues[1].real())
+            {
+                larger = eigenvectors.col(0).real();
+                smaller = eigenvectors.col(1).real();
+            }
+            else
+            {
+                larger = eigenvectors.col(1).real();
+                smaller = eigenvectors.col(0).real();
+            }
+
+            Eigen::Matrix2d ue_attachment;
+            ue_attachment <<
+                projected_velocity[0], smaller[0],
+                projected_velocity[1], smaller[1];
+
+            Eigen::Matrix2d ue_separation;
+            ue_separation <<
+                projected_velocity[0], larger[0],
+                projected_velocity[1], larger[1];
+
+            attachment_values->SetValue(i, ue_attachment.determinant());
+            separation_values->SetValue(i, ue_separation.determinant());
+
+            // Transform eigenvectors back to 3D space and store them
+            Eigen::Matrix3d basis_transformation;
+            basis_transformation << basis_1, basis_2, normal;
+
+            const Eigen::Vector3d attachment_vector = basis_transformation * Eigen::Vector3d(smaller[0], smaller[1], 0.0);
+            const Eigen::Vector3d separation_vector = basis_transformation * Eigen::Vector3d(larger[0], larger[1], 0.0);
+
+            attachment_eigenvectors->SetTuple(i, attachment_vector.data());
+            separation_eigenvectors->SetTuple(i, separation_vector.data());
+        }
+        else
+        {
+            // Set recognizable dummy values
+            attachment_values->SetValue(i, std::nan(""));
+            separation_values->SetValue(i, std::nan(""));
+
+            attachment_eigenvectors->SetTuple3(i, 0.0, 0.0, 0.0);
+            separation_eigenvectors->SetTuple3(i, 0.0, 0.0, 0.0);
+        }
     }
 
     // Create output
@@ -141,6 +214,10 @@ int attachment_separation_lines::RequestData(vtkInformation*, vtkInformationVect
 
     output->ShallowCopy(input);
     output->GetPointData()->AddArray(values);
+    output->GetPointData()->AddArray(attachment_values);
+    output->GetPointData()->AddArray(separation_values);
+    output->GetPointData()->AddArray(attachment_eigenvectors);
+    output->GetPointData()->AddArray(separation_eigenvectors);
 
     return 1;
 }
